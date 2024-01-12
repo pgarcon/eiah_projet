eb87216778f1c9c7c10fc494d9d90aab42e8db1c
Lemoult Nicolas <s186244@ic2s114-11.univ-lemans.fr>
2024-01-12 14:22:00 +0100

    correct classification

--- model/classification.py
+++ model/classification.py
@@ -18,18 +18,19 @@ os.makedirs(output_folder, exist_ok=True)
 data.reset_index(drop=True, inplace=True)
 
 # Sélectionner les colonnes nécessaires pour le clustering
-features = data[['nb_traces', 'nb_remove', 'tmp_moy', 'cp_pst', 'nb_save', 'quantite', 'nb_gcc_error']]
+donnee = data[['nb_traces', 'nb_remove', 'tmp_moy', 'cp_pst', 'nb_save', 'quantite', 'nb_gcc_error']]
 
 # Normaliser les données
 scaler = StandardScaler()
-features_scaled = scaler.fit_transform(features)
+donnee_scale = scaler.fit_transform(donnee)
 
 # Appliquer l'algorithme KMeans pour obtenir les clusters
 kmeans = KMeans(n_clusters=nb_cluster)
-data['cluster'] = kmeans.fit_predict(features_scaled)
+data['cluster'] = kmeans.fit_predict(donnee_scale)
 
 # Sauvegarder chaque cluster dans un fichier CSV
 for cluster_id in range(nb_cluster):
     cluster_data = data[data['cluster'] == cluster_id]
+    cluster_data.drop('cluster', axis=1, inplace=True)
     cluster_file_path = os.path.join(output_folder, f"cluster_{cluster_id + 1}_data.csv")
     cluster_data.to_csv(cluster_file_path, index=False)
